6eb869edf4e84b28ecebc76912c9bbadf68f8ca6
Pen9uin <sirpen9uin@gmail.com>
2024-07-07 14:57:09 +0300

    init

---
+++ src/js/__tests__/user.test.js
@@ -0,0 +1,24 @@
+import { loadUser, playersSorted } from '../user';
+import { httpGet } from '../http';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should call loadUser once', () => {
+  httpGet.mockReturnValue(JSON.stringify({}));
+
+  const response = loadUser(1);
+  expect(response).toEqual({});
+  expect(httpGet).toHaveBeenCalledWith('http://server:8080/users/1');
+});
+
+test('sorted', () => {
+  expect(playersSorted).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
